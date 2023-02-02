ee910c4fc532b77f2a65cee9802d1146389aae01
Juan David <120610426+JUAN57755@users.noreply.github.com>
2023-02-02 18:57:01 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+-7yg8rjehvndrght8yenvjhrgyt98rvdfr9t5ugjnr8utgrnvkldjieo4ut89tugrkvjrout4987yghvkl0r9pw09rp-0-4e46=-4t4vt*&$^%*^&^$%^&UYYTHVgy4g%RTBR%VRYVG^%TRFRFTvTV6VTF
+rtfvV
+TvF^YRT
+vf%
+TRC4%
+CR%CTD
+5d4^CTr
+5FRC
+W$%
+ERf^T
+RF
+%FTRY
+^frCT%
+rf^t
+5rFR
+65tGV^Y^
+T6&
+g6
+6
+yy6ytG%tr55rjnikuniveo4fse srji9of hfjrnfoi4fjkjfei3eidjcnfe4okprv hu3ejkfiur4eijfou8934jiorkmfldoi9feorlkmefio9deo4j3ki9er4k5o9r0ie4i950894ruio45094okr4pi59i4o4905i4topi596o5t5
+tt
+5tg
+t65rg5t4t
+grgr54gGRT%TR5yHTRY%$R%YHTGT$TRBFRGT#GRF$TGBGDFR$T%HTgbT%6%THBGR$54RR$egt%GbR%YJ^YHYu76YJhtr6uYH^%UYTUJYTH^%TRHY%TRGFR
